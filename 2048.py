eb10d89215f8a11ee41c689b69de0312681ffec4
rean.wen <reaniwen@gmail.com>
2014-04-01 14:17:02 -0400

    prototype
    only support direction move

---
+++ 2048.py
@@ -0,0 +1,68 @@
+Board= [[0]*4 for row in range(4)]
+def print_board(Board):
+	for row in Board:
+		for cell in row:
+			print cell,
+		print
+	print
+
+def judge_dir(drt):
+	if drt == "w":
+		for col in range (4):
+			for row in range(4):
+				space = 0
+				if Board[row][col]!=0:
+					Board[space][col] = Board[row][col]
+					if space != row:
+						Board[row][col] = 0
+						space += 1
+	elif drt == 's':
+		for col in range(4):
+			for row in range(3,-1,-1):
+				#row = 3-neg_row
+				space = 3
+				if Board[row][col]!=0:
+					Board[space][col] = Board[row][col]
+					if space != row:
+						Board[row][col] = 0
+						space -= 1
+	elif drt == "a":
+		for row in range (4):
+			for col in range(4):
+				space = 0
+				if Board[row][col]!= 0:
+					Board[row][space] = Board[row][col]
+					if space !=	col:
+						Board[row][col] = 0
+						space += 1
+	elif drt == 'd':
+		for row in range (4):
+			for col in range(3,-1,-1):
+				#col = 3-neg_col
+				space = 3
+				if Board[row][col]!= 0:
+					Board[row][space] = Board[row][col]
+					if space!= col:
+						Board[row][col] = 0
+						space -= 1
+
+#def utod(oneLine):
+#	i,j=0,1
+#	while j!=4:
+#		oneLine[]
+
+#def dtou():
+					
+
+print_board(Board)
+#i = input('How many:')
+x,y = input('location:')
+Board[x][y]= 1
+print_board(Board)
+while True:
+	drt = raw_input("direction:")
+	judge_dir(drt)
+	print_board(Board)
+	x,y = input('location:')
+	Board[x][y]= 1
+	print_board(Board)
